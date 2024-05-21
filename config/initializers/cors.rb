5f5b0b2db72ad5113e760e8bc4da70c68f4b1b0d
Takuma <>
2024-05-21 18:43:47 +0900

    .env urlの修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://react-api-chatgpt.vercel.app/'
+      origins 'https://react-api-chatgpt.vercel.app'
   
       resource '*',
         headers: :any,
