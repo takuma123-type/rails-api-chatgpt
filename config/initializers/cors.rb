cf086bb5912db3fb4dfef4b981c22835f11ca900
Takuma <>
2024-05-21 19:32:15 +0900

    フロントのURL修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://react-api-chatgpt.vercel.app'
+      origins 'https://chatgpt-aura-bot.soru-tech.com'
   
       resource '*',
         headers: :any,
